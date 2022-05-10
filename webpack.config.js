a373cadae7df3727cc90b451e0d239311ce4a8e5
John Whitten <johnwhitten.studio@gmail.com>
2022-05-09 20:41:00 -0700

    create index.html and update webpack and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require("dotenv-webpack");
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -19,7 +20,8 @@ module.exports = {
       title: 'jsDevEnvironmentTemplate',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: { 
     rules: [
